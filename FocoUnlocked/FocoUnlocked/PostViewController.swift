d8c983237fd947dad8962f00715d1943946faf18
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-12-05 20:52:05 -0800

    checks to see if post is valid

--- FocoUnlocked/FocoUnlocked/PostViewController.swift
+++ FocoUnlocked/FocoUnlocked/PostViewController.swift
@@ -27,8 +27,30 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     }
     @IBAction func savePost(_ sender: Any) {
         // Creating new post object
-        let newPost = Post(username: "Kendrick Lamar", time: NSDate(), dishName: dishTitle.text!, image: UIImagePNGRepresentation(pickedImage.image!)! as NSData, likes: 0);
+        let currentTime = NSDate()
+        if (isPostValid(username: "Kendrick Lamar", dishName: dishTitle, image: pickedImage) != 0) {
+            print("This isn't a valid post")
+            return
+        }
+        let newPost = Post(username: "Kendrick Lamar", time: currentTime, dishName: dishTitle.text!, image: UIImagePNGRepresentation(pickedImage.image!)! as NSData, likes: 0);
         print(newPost.toString());
+    }
+    
+    // checks if post is valid
+    func isPostValid(username: String, dishName: UITextField, image: UIImageView) -> Int {
+        if (username.isEmpty) {
+            print("Invalid username")
+            return -1
+        }
+        if (dishName.text!.isEmpty) {
+            print("Enter a proper dish title")
+            return -2
+        }
+        if (image.image == nil || (UIImagePNGRepresentation(image.image!)! as NSData).length == 0) {
+            print("Provide image")
+            return -3
+        }
+        return 0;
     }
     
     // opens the gallery for user to choose image
